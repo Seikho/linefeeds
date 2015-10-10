bddfa225351e25d9fe205e4a9a8492e78ea22972
Carl <carl@carlossus.com>
2015-10-10 10:29:04 +0800

    Bump minor version

--- test/index.ts
+++ test/index.ts
@@ -16,7 +16,7 @@ describe('conversion tests', () => {
     it('will replace all line endings with CRLF', () => {
         LF.convertSync('tsconfig.json', { target: 'test.txt', ending: LF.ending.crlf });
 
-        expect(test('test.txt', cr)).to.be.true;       
+        expect(test('test.txt', cr)).to.be.true;
     });
 
     it('will replace all line endings with LF', () => {
@@ -53,32 +53,32 @@ describe('async tests', () => {
 });
 
 describe('stream tests', () => {
-   it('will convert line endings to CRLF with streams', done => {
-      var stream = LF.stream('tsconfig.json', { ending: LF.ending.crlf });
-      var buffer = '';
-      stream.on('end', () => {          
-          expect(cr.test(buffer)).to.be.true;
-          done();
-      });
-      
-      stream.on('data', data => {
-          buffer += data;
-      });
-   });
-   
-   it('will convert line endings to LF with streams', done => {
-      var stream = LF.stream('tsconfig.json', { ending: LF.ending.lf });
-      var buffer = '';
-      stream.on('end', () => {          
-          expect(cr.test(buffer)).to.be.false;
-          expect(lf.test(buffer)).to.be.true;
-          done();
-      });
-      
-      stream.on('data', data => {
-          buffer += data;
-      });
-   });
+    it('will convert line endings to CRLF with streams', done => {
+        var stream = LF.stream('tsconfig.json', { ending: LF.ending.crlf });
+        var buffer = '';
+        stream.on('end', () => {
+            expect(cr.test(buffer)).to.be.true;
+            done();
+        });
+
+        stream.on('data', data => {
+            buffer += data;
+        });
+    });
+
+    it('will convert line endings to LF with streams', done => {
+        var stream = LF.stream('tsconfig.json', { ending: LF.ending.lf });
+        var buffer = '';
+        stream.on('end', () => {
+            expect(cr.test(buffer)).to.be.false;
+            expect(lf.test(buffer)).to.be.true;
+            done();
+        });
+
+        stream.on('data', data => {
+            buffer += data;
+        });
+    });
 });
 
 function test(file: string, regex: RegExp) {
